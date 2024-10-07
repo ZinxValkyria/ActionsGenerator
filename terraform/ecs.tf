21551a15aa618506399cf2c45deb707e92d7117c
Blain Noone <nooneblain@gmail.com>
2024-10-07 12:27:54 +0100

    working without css

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -84,7 +84,7 @@ resource "aws_ecs_task_definition" "ecs_task" {
 [
   {
     "name": "actions-generator",
-    "image": "zinx666/actions_generator:latest",
+    "image": "zinx666/actions_generator:9f6c8d2",
     "essential": true,
     "portMappings": [
       {
@@ -158,6 +158,20 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app_lb.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_target_group.arn
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  ssl_policy      = "ELBSecurityPolicy-2016-08"
+  certificate_arn = "arn:aws:acm:eu-west-1:188132471158:certificate/01dfe950-0751-4f57-9e4e-2f13ea9f0900"
 
   default_action {
     type             = "forward"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,20 +1,20 @@
 terraform {
   backend "s3" {
-    bucket         = "actions-template-state"
-    key            = "global/mystatefile/terraform.tfstate"
-    region         = "eu-west-2"
+    bucket = "actions-template-state"
+    key    = "global/mystatefile/terraform.tfstate"
+    region = "eu-west-2"
     # Uncomment the next line if you want to use DynamoDB for state locking
     # dynamodb_table = "state-lock"
   }
 
   required_providers {
     cloudflare = {
-      source  = "cloudflare/cloudflare"  # Correct source address for Cloudflare
-      version = "~> 3.0"  # Specify the version you want
+      source  = "cloudflare/cloudflare" # Correct source address for Cloudflare
+      version = "~> 3.0"                # Specify the version you want
     }
     aws = {
-      source  = "hashicorp/aws"  # AWS provider remains as it is
-      version = "~> 5.0"  # Specify the version you want
+      source  = "hashicorp/aws" # AWS provider remains as it is
+      version = "~> 5.0"        # Specify the version you want
     }
   }
 }
@@ -24,12 +24,12 @@ provider "cloudflare" {
 }
 
 resource "cloudflare_record" "app_record" {
-  zone_id = var.cloudflare_zone_id  # Reference a variable for Zone ID
-  name     = "app"                   # This will create app.zinxvalkyria.space
-  value    = aws_lb.app_lb.dns_name  # Points to the ALB DNS name
-  type     = "CNAME"                 # Change to CNAME since you're pointing to a DNS name
-  ttl      = 300                     # Time to live in seconds
-  proxied  = false                   # Set to true if you want Cloudflare's proxy features
+  zone_id = var.cloudflare_zone_id # Reference a variable for Zone ID
+  name    = "app"                  # This will create app.zinxvalkyria.space
+  value   = aws_lb.app_lb.dns_name # Points to the ALB DNS name
+  type    = "CNAME"                # Change to CNAME since you're pointing to a DNS name
+  ttl     = 300                    # Time to live in seconds
+  proxied = false                  # Set to true if you want Cloudflare's proxy features
 }
 
 
