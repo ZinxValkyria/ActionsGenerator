9852d8c8c9a4a0876a7601f50fe167f7f1b33990
Blain <dead3165@gmail.com>
2024-10-08 18:47:22 +0100

    update readme

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ from dotenv import load_dotenv
 
 app = Flask(__name__, template_folder="templatesv2")
 load_dotenv()
-# Define the directory where local YAML scripts are stored
+
+#  YAML scripts are stored
 SCRIPTS_DIR = os.path.join(os.getcwd(), "scripts")
 
-# Ensure that the scripts directory exists
+# Ensure scripts directory exists
 if not os.path.exists(SCRIPTS_DIR):
     raise FileNotFoundError(
         f"The directory {SCRIPTS_DIR} does not exist. Please check your project structure."
@@ -118,11 +119,11 @@ def fetch_yaml(filename):
     """
     file_path = os.path.join(SCRIPTS_DIR, filename)
 
-    # Check if the file exists before sending it
+    # Check if the file exists 
     if os.path.isfile(file_path):
         return send_from_directory(SCRIPTS_DIR, filename)
 
-    # If the file doesn't exist, return a 404 error
+    # If the file doesn't exist, return error
     return abort(404, description="File not found")
 
 
