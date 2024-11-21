41bbf937c31d70a55ec4a5d6e4f72d8afde14400
Blain <dead3165@gmail.com>
2024-11-21 23:39:41 +0000

    various tweaks

--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 # Use the official Python image from the Docker Hub
-FROM python:3.11-alpine
+FROM python:3.8-slim
 
 # Set the working directory in the container
 WORKDIR /app
@@ -13,6 +13,12 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Copy the rest of the application code into the container
 COPY . .
 
+# Copy the .env file into the container
+COPY .env /app/.env
+
+# Copy the New Relic configuration file
+COPY newrelic.ini /app/newrelic.ini
+
 # Expose port 5000 for the Flask application
 EXPOSE 5000
 
@@ -21,5 +27,5 @@ ENV FLASK_APP=app.py
 ENV FLASK_RUN_HOST=0.0.0.0
 ENV FLASK_RUN_PORT=5000
 
-# Run the Flask application
-CMD ["flask", "run"]
+# Run the Flask application with New Relic monitoring
+CMD ["newrelic-admin", "run-program", "python3", "app.py"]
--- terraform/logs.tf
+++ terraform/logs.tf
@@ -1,42 +1,42 @@
-resource "aws_iam_role" "firehose_role" {
-  name = "firehose-log-forwarding-role"
+# resource "aws_iam_role" "firehose_role" {
+#   name = "firehose-log-forwarding-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "firehose.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "firehose.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "firehose_policy" {
-  role       = aws_iam_role.firehose_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonKinesisFirehoseFullAccess"
-}
+# resource "aws_iam_role_policy_attachment" "firehose_policy" {
+#   role       = aws_iam_role.firehose_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonKinesisFirehoseFullAccess"
+# }
 
-resource "aws_kinesis_firehose_delivery_stream" "log_forwarding_stream" {
-  name        = "cloudwatch-to-newrelic-stream"
-  destination = "http_endpoint"
+# resource "aws_kinesis_firehose_delivery_stream" "log_forwarding_stream" {
+#   name        = "cloudwatch-to-newrelic-stream"
+#   destination = "http_endpoint"
 
-  http_endpoint_configuration {
-    url        = "https://log-api.newrelic.com/log/v1" # New Relic Log API endpoint
-    access_key = var.new_relic_license_key             # New Relic Insert API Key
-    name       = "NewRelicLogEndpoint"                 # Required field to provide a valid name
+#   http_endpoint_configuration {
+#     url        = "https://log-api.newrelic.com/log/v1" # New Relic Log API endpoint
+#     access_key = var.new_relic_license_key             # New Relic Insert API Key
+#     name       = "NewRelicLogEndpoint"                 # Required field to provide a valid name
 
-    s3_configuration {
-      role_arn           = aws_iam_role.firehose_role.arn
-      bucket_arn         = "arn:aws:s3:::newrelictest2"
-      buffering_interval = 300 # in seconds
-      buffering_size     = 5   # in MB
-      compression_format = "UNCOMPRESSED"
-    }
+#     s3_configuration {
+#       role_arn           = aws_iam_role.firehose_role.arn
+#       bucket_arn         = "arn:aws:s3:::newrelictest2"
+#       buffering_interval = 300 # in seconds
+#       buffering_size     = 5   # in MB
+#       compression_format = "UNCOMPRESSED"
+#     }
 
-    role_arn = aws_iam_role.firehose_role.arn
-  }
-}
+#     role_arn = aws_iam_role.firehose_role.arn
+#   }
+# }
