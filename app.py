6ed948804627f6a46a9f240445fd2c630ca3401a
Blain <97914052+ZinxValkyria@users.noreply.github.com>
2024-11-22 01:12:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ from flask import Flask, render_template, send_from_directory, abort
 import newrelic.agent  # Import New Relic Agent
 from dotenv import load_dotenv
 
-newrelic.agent.initialize('newrelic.ini')  # Ensure this points to your actual `newrelic.ini` file
-
 app = Flask(__name__, template_folder="templatesv2")
 load_dotenv()
 
