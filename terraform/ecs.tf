a4623856d8e50916b10bc33796278812169ba0d8
Blain Noone <nooneblain@gmail.com>
2024-12-09 16:44:40 +0000

    hotfix for image tag

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -43,7 +43,7 @@ resource "aws_ecs_task_definition" "ecs_task" {
   container_definitions = jsonencode([
     {
       "name" : "actions-generator",
-      "image" : "zinx666/actions_generator:2bb6fa8",
+      "image" : "zinx666/actions_generator:${var.image_tag}",
       "essential" : true,
       "portMappings" : [
         {
