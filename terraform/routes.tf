0cd19aace27b399ec4a1208061f486ee0d5169bc
blain <dead3165@gmail.com>
2024-12-14 00:10:28 +0000

    access.yf

--- terraform/routes.tf
+++ terraform/routes.tf
@@ -14,22 +14,7 @@ resource "aws_route_table_association" "rt_asc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# Create a route table for the private subnet
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.my_nat_gw.id
-  }
-
-  tags = {
-    Name = "Private Route Table"
-  }
-}
-
-# Associate the private route table with the private subnet
-resource "aws_route_table_association" "private_association" {
-  subnet_id      = aws_subnet.private_sub.id
-  route_table_id = aws_route_table.private_rt.id
+resource "aws_route_table_association" "rt_asc_sub2" {
+  subnet_id      = aws_subnet.public_sub2.id
+  route_table_id = aws_route_table.public_rt.id
 }
