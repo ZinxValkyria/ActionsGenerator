8d351ad8295eaaaf0c0033559929d9d914a2549e
blain <dead3165@gmail.com>
2024-12-14 17:39:21 +0000

    load balancer securitry group

---
+++ terraform/loadbalancer.tf
@@ -0,0 +1,34 @@
+# Define security group for the Load Balancer
+resource "aws_security_group" "app_lb_sg" {
+  vpc_id = aws_vpc.main.id
+
+  # Allow HTTP and HTTPS traffic from anywhere (can be restricted later)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP traffic from the internet
+    description = "Allow HTTP traffic from the internet"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTPS traffic from the internet
+    description = "Allow HTTPS traffic from the internet"
+  }
+
+  # Allow outbound traffic to ECS instances (private subnet)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "Load Balancer Security Group"
+  }
+}
