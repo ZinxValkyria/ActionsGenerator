3a372625698d08a378026d5f2cfc342c681758ac
blain <dead3165@gmail.com>
2024-12-18 23:11:03 +0000

    hotfix, removed acls

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,15 +1,15 @@
-# DynamoDB table created for state-locking
-resource "aws_dynamodb_table" "statelock" {
-  name         = "state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+# # DynamoDB table created for state-locking
+# resource "aws_dynamodb_table" "statelock" {
+#   name         = "state-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+#   lifecycle {
+#     prevent_destroy = true
+#   }
 
-}
+# }
