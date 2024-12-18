df7cb22f851326e14d9e9278534392b43b817032
blain <dead3165@gmail.com>
2024-12-18 19:13:27 +0000

    added load bal;ancer security groups

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -1,34 +1,66 @@
-# Define security group for the Load Balancer
-resource "aws_security_group" "app_lb_sg" {
-  vpc_id = aws_vpc.main.id
 
-  # Allow HTTP and HTTPS traffic from anywhere (can be restricted later)
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP traffic from the internet
-    description = "Allow HTTP traffic from the internet"
-  }
+# Create Target Group with IP target type
+resource "aws_lb_target_group" "ecs_target_group" {
+  name        = "ecs-target-group"
+  port        = 5000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow HTTPS traffic from the internet
-    description = "Allow HTTPS traffic from the internet"
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
+}
 
-  # Allow outbound traffic to ECS instances (private subnet)
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
-    description = "Allow all outbound traffic"
-  }
+# Create Load Balancer
+resource "aws_lb" "app_lb" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.app_lb_sg.id]
+
+  subnets = [
+    aws_subnet.public_sub1.id,
+    aws_subnet.public_sub2.id
+  ]
+
+  enable_deletion_protection = false
 
   tags = {
-    Name = "Load Balancer Security Group"
+    Name = "App Load Balancer"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      protocol    = "HTTPS"
+      port        = "443"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# Listener for the load balancer on https
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn = "arn:aws:acm:eu-west-1:188132471158:certificate/01dfe950-0751-4f57-9e4e-2f13ea9f0900"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_target_group.arn
   }
 }
